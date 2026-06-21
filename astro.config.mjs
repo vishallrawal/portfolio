3ac3fd088c0157250205507c3ac12fb43c4a6613
vishallrawal <vishalrawal448@gmail.com>
2026-06-21 22:05:30 +0530

    Configure GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
+  site: "https://vishallrawal.github.io",
+  base: "/portfolio",
+
   vite: {
     plugins: [tailwindcss()],
   },
